fe92b9a5b794931b8ccbcbfe9054ea5cd85b2496
matthewkim1005 <matt_hew_kim@yahoo.com>
2024-07-17 01:20:09 -0700

    moved pseudocode to a different page, cleaned up code as well.

---
+++ script-files/planning.js
@@ -0,0 +1,46 @@
+//Pseudocode:
+
+// 1. Getting a list of 5 letter words to pull from and selecting a random one
+
+// 2. Creating a 5x? grid in the HTML for the GUI.
+
+// 3. Taking user input from the keyboard or the on screen keyboard.
+
+// 4. Taking in a user's 5 letter word and comparing it to the randomly chosen word.
+
+// 5. Checking each letter and the index of that letter to see if it matches the word.
+
+// 6. Giving the user graphical cues to show which letters are in the right/wrong place.
+
+// 7. Letting the user know that they won by guessing the correct word.
+
+// 8. Letting the user know IF they have run out of guesses and lost.
+
+// 9. Play again, help, and reset functionality.
+
+// 10. Verify if it is a word in the list. If word is invalid or not.
+
+//------------------------------------------------------------------------------------------------------------------
+
+// Define the random 5-letter word from a list of 5-letter words, this will be LET.
+
+// Define the letters that the user has guessed, LET.
+
+// Define the letters that are correct, in contained in the word, and wrong.
+
+// User inputs 5 letters
+    //IF all 5 letter match the squence of letters of the word THEN user wins the game.
+    //IF a letter is out of sequence, but is in the word, make it YELLOW.
+        //turn words on the on-screen keyboard YELLOW as well.
+    //IF a letter is in the correct sequence, make it GREEN.
+        //turn words on the on-screen keyboard GREEN as well.
+    //IF a letter is not in the word at all, make it GRAY.
+        //turn words on the on-screen keyboard GRAY as well.
+
+// User will get 6 tries to guess the word.
+
+//------------------------------------------------------------------------------------------------------------------
+
+// (Extra): Sound effects.
+
+//------------------------------------------------------------------------------------------------------------------
--- script-files/script.js
+++ script-files/script.js
@@ -1,53 +1,4 @@
-//Pseudocode:
-
-// 1. Getting a list of 5 letter words to pull from and selecting a random one
-
-// 2. Creating a 5x? grid in the HTML for the GUI.
-
-// 3. Taking user input from the keyboard or the on screen keyboard.
-
-// 4. Taking in a user's 5 letter word and comparing it to the randomly chosen word.
-
-// 5. Checking each letter and the index of that letter to see if it matches the word.
-
-// 6. Giving the user graphical cues to show which letters are in the right/wrong place.
-
-// 7. Letting the user know that they won by guessing the correct word.
-
-// 8. Letting the user know IF they have run out of guesses and lost.
-
-// 9. Play again, help, and reset functionality.
-
-// 10. Verify if it is a word in the list. If word is invalid or not.
-
-//------------------------------------------------------------------------------------------------------------------
-
-// Define the random 5-letter word from a list of 5-letter words, this will be LET.
-
-// Define the letters that the user has guessed, LET.
-
-// Define the letters that are correct, in contained in the word, and wrong.
-
-// User inputs 5 letters
-    //IF all 5 letter match the squence of letters of the word THEN user wins the game.
-    //IF a letter is out of sequence, but is in the word, make it YELLOW.
-        //turn words on the on-screen keyboard YELLOW as well.
-    //IF a letter is in the correct sequence, make it GREEN.
-        //turn words on the on-screen keyboard GREEN as well.
-    //IF a letter is not in the word at all, make it GRAY.
-        //turn words on the on-screen keyboard GRAY as well.
-
-// User will get 6 tries to guess the word.
-
-//------------------------------------------------------------------------------------------------------------------
-
-// (Extra): Scoreboard: Keeps track of words guessed wrong/right.
-// (Extra): Sound effects.
-
-//------------------------------------------------------------------------------------------------------------------
-
 import { fiveLetterWords } from './5-letter-words.js';
-// fiveLetterWords = require('./5-letter-words.js');
 
 // Variables -------------------------------------------------------------------------------------------------------
 
@@ -205,7 +156,6 @@ keys.forEach((key) => {
 
   function checkWord() {
     winMessage.innerHTML = '';
-    // let index = numGuesses * 5;
     let index = userInput.length - 5;
     let tempWord = randomWord.toUpperCase();;
     currentWord = userInput.substr(userInput.length - 5).toUpperCase();
@@ -286,6 +236,7 @@ keys.forEach((key) => {
     init();
   }
 
+  //ends game if more than 6 guesses
   function gameEnd() {
     if (userInput.length > 29 && !won) {
       winMessage.innerHTML = `You lost! The word is ${randomWord}`;
@@ -354,7 +305,3 @@ keys.forEach((key) => {
     gameGrid.childNodes[userInput.length-5].classList.remove("apply-shake");
     });
   } 
-
-  //make onscreen keyboard work
-  //add help menu
-  
